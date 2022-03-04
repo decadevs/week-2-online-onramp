40b22a15d6546157d3e898a028357a40fb7132a5
Chesca22 <99410162+Chesca22@users.noreply.github.com>
2022-03-04 20:37:39 +0100

    week-2-Onramp Assignment

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,15 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+  let arrayOfIntegers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let newArr = [];
+  if (numArr.length === arrayOfIntegers.length) {
+    return false;
+  } else {
+    numArr.sort((a, b) => a - b);
+    newArr = arrayOfIntegers.filter(function (obj) {
+      return numArr.indexOf(obj) === -1;
+    });
+    return newArr;
+  }
+}
 
-module.exports = missingNumber
+module.exports = missingNumber;
