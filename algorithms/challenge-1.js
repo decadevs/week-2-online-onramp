15fe5d13af30fa7037b07b70b56c83f76887c7ca
positivelizzy <55453747+positivelizzy@users.noreply.github.com>
2022-03-06 14:09:24 +0100

    Update challenge-1.js
    solution to challenge 1

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,19 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+  count = 9,
+  missing = []
+
+  for (let i = 1; i <= count; i++) {
+    if (numArr.indexOf(i) === -1) {
+      missing.push(i)
+    }
+  }
+  if(missing.length ===0){ // if missing array is empty
+    return false;
+  }else if(missing.length === 1){ // if missing array has length of 1
+    return missing[0];
+  }else {
+    return missing;
+  }
+}
 
 module.exports = missingNumber
