fcaac41fcd4009ccb3a7f24fc9c5563f86ab4b00
shittu wasiu <110758549+Abdulwasiu628@users.noreply.github.com>
2022-11-17 05:43:26 +0100

    Challenge-1 done

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,27 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+  var emptyArray = new Array()
+    var newList = new Array()
+    var x = 0
+    for(i=0; i<=numArr.length-1; i++){
+        newList.push(numArr[i])
+    }
+    for(i=1; i<=9; i++){
+        if(newList.includes(i) == false){
+            emptyArray.push(i)
+        }
+        
+    }
+    if(emptyArray.length == 1){
+        return emptyArray[0];
+    }
+    else if(emptyArray.length > 1){
+        return emptyArray;
+    }
+    else{
+        return false;
+    }
+
+
+}
 
 module.exports = missingNumber
